0eda5eb34c9a4d40580975ad634c70a41ffdd6fa
Ratthanan <ratthanan.p@codium.co>
2020-10-07 13:58:57 +0700

    Numbers bigger than 1000 should be ignored

--- main.ts
+++ main.ts
@@ -1,11 +1,28 @@
-export function handleNewLine(string: string): string {
+
+
+function handleNewLine(string: string): string {
     var newString = string.replace("\n", ","); 
     return newString
 }
 
-export function handleDelimiters(string: string): string {
+function handleDelimiters(string: string): string {
     var newString = string.replace(/.*\n/, ';')
     return newString
+}
+
+function removeNumbersBiggerThan(array: number[]): number[] {
+    var new_arr = array.filter(function(x) {
+        return x < 1000;
+    });
+    return new_arr
+}
+
+function negativesNotAllowed(string: string): void {
+    // const regex = "^-\d+$";
+    // var isNegatives = regex.test(string);
+    // if (isNegatives) {
+        throw Error("Negatives not allowed")
+    // }
 }
 
 
@@ -16,11 +33,13 @@ export function intAdd(string: string) : number{
         return answer
     }
 
+    // negativesNotAllowed(string);
+
     // Allow the Add method to handle new lines between numbers
     var string = handleNewLine(string);
-
+    
     // Support different delimiters
-    var string = handleDelimiters(string);
+    // var string = handleDelimiters(string);
 
     // Split String to array
     var splitNumber = string.split(",").map(function(item) {
@@ -28,7 +47,10 @@ export function intAdd(string: string) : number{
     });
 
     listNumber = listNumber.concat(splitNumber);
-    
+
+    // Numbers bigger than 1000 should be ignored
+    listNumber = removeNumbersBiggerThan(listNumber);
+
     // Sum Array
     answer = listNumber.reduce((a, b) => a + b, 0)
     return answer
